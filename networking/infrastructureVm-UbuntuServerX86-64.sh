6c87c340cc64ce0e6c0a3882723ae73cc55404fb
reubenajohnston <reuben.a.johnston@gmail.com>
2024-03-01 08:34:51 -0500

    Update infrastructureVm-UbuntuServerX86-64.sh

--- networking/infrastructureVm-UbuntuServerX86-64.sh
+++ networking/infrastructureVm-UbuntuServerX86-64.sh
@@ -11,7 +11,7 @@ git init
 git config pull.rebase false  # merge (the default strategy)
 
 # Track repository, do not enter subdirectory
-git remote add -f origin https://github.com/jhu-information-security-institute/NwSec.git
+git remote add -f origin https://github.com/jhu-information-security-institute/infrastructure.git
 
 # Enable the tree check feature
 git config core.sparseCheckout true
@@ -20,20 +20,20 @@ git config core.sparseCheckout true
 # That is inside the hidden .git directory that was created
 # by running the command: git init
 # And inside it enter the name of the specific files (or subdirectory) you only want to clone
-echo 'config/UbuntuServerX86-64/.project' >> .git/info/sparse-checkout
-echo 'config/UbuntuServerX86-64/infrastructureVm-README.md' >> .git/info/sparse-checkout
-echo 'config/UbuntuServerX86-64/infrastructureVm-install1.sh' >> .git/info/sparse-checkout
-echo 'config/UbuntuServerX86-64/infrastructureVm-install2.sh' >> .git/info/sparse-checkout
-echo 'config/UbuntuServerX86-64/infrastructureVm-remove.sh' >> .git/info/sparse-checkout
-echo 'config/UbuntuServerX86-64/warmstart-netplan.sh' >> .git/info/sparse-checkout
-echo 'config/UbuntuServerX86-64/prepshutdown-netplan.sh' >> .git/info/sparse-checkout
-echo 'config/UbuntuServerX86-64/etc_netplan_99-config.yaml' >> .git/info/sparse-checkout
-echo 'config/UbuntuServerX86-64/etc_netplan_100-config.yaml' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/.project' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/infrastructureVm-README.md' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/infrastructureVm-install1.sh' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/infrastructureVm-install2.sh' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/infrastructureVm-remove.sh' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/warmstart-netplan.sh' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/prepshutdown-netplan.sh' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/etc_netplan_99-config.yaml' >> .git/info/sparse-checkout
+echo 'networking/UbuntuServerX86-64/etc_netplan_100-config.yaml' >> .git/info/sparse-checkout
 
 ## Download with pull, not clone
-git pull origin master
+git pull origin main
 
-echo 'cd into infrastructureVm/config/UbuntuServerX86-64 and view details in infrastructureVm-README.md'
+echo 'cd into infrastructureVm/networking/UbuntuServerX86-64 and view details in infrastructureVm-README.md'
 
 # References:
 #   https://terminalroot.com/how-to-clone-only-a-subdirectory-with-git-or-svn
