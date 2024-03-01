2641678f48767e402fde62cb8bb8f09c85825caa
reubenajohnston <reuben.a.johnston@gmail.com>
2024-03-01 08:32:07 -0500

    Update dnssvr-UbuntuServerX86-64.sh

--- networking/dnssvr/dnssvr-UbuntuServerX86-64.sh
+++ networking/dnssvr/dnssvr-UbuntuServerX86-64.sh
@@ -11,7 +11,7 @@ git init
 git config pull.rebase false  # merge (the default strategy)
 
 # Track repository, do not enter subdirectory
-git remote add -f origin https://github.com/jhu-information-security-institute/NwSec.git
+git remote add -f origin https://github.com/jhu-information-security-institute/infrastructure.git
 
 # Enable the tree check feature
 git config core.sparseCheckout true
@@ -20,19 +20,19 @@ git config core.sparseCheckout true
 # That is inside the hidden .git directory that was created
 # by running the command: git init
 # And inside it enter the name of the specific files (or subdirectory) you only want to clone
-echo 'applications/dnssvr/UbuntuServerX86-64/.project' >> .git/info/sparse-checkout
-echo 'applications/dnssvr/UbuntuServerX86-64/Dockerfile' >> .git/info/sparse-checkout
-echo 'applications/dnssvr/UbuntuServerX86-64/etc_bind_named.conf.local' >> .git/info/sparse-checkout
-echo 'applications/dnssvr/UbuntuServerX86-64/etc_bind_named.conf.options' >> .git/info/sparse-checkout
-echo 'applications/dnssvr/UbuntuServerX86-64/etc_bind_zones_db.25.168.192' >> .git/info/sparse-checkout
-echo 'applications/dnssvr/UbuntuServerX86-64/etc_bind_zones_db.netsec-docker.isi.jhu.edu' >> .git/info/sparse-checkout
-echo 'applications/dnssvr/UbuntuServerX86-64/etc_default_named' >> .git/info/sparse-checkout
-echo 'applications/dnssvr/UbuntuServerX86-64/etc_resolv.conf' >> .git/info/sparse-checkout
+echo 'networking/dnssvr/UbuntuServerX86-64/.project' >> .git/info/sparse-checkout
+echo 'networking/dnssvr/UbuntuServerX86-64/Dockerfile' >> .git/info/sparse-checkout
+echo 'networking/dnssvr/UbuntuServerX86-64/etc_bind_named.conf.local' >> .git/info/sparse-checkout
+echo 'networking/dnssvr/UbuntuServerX86-64/etc_bind_named.conf.options' >> .git/info/sparse-checkout
+echo 'networking/dnssvr/UbuntuServerX86-64/etc_bind_zones_db.25.168.192' >> .git/info/sparse-checkout
+echo 'networking/dnssvr/UbuntuServerX86-64/etc_bind_zones_db.netsec-docker.isi.jhu.edu' >> .git/info/sparse-checkout
+echo 'networking/dnssvr/UbuntuServerX86-64/etc_default_named' >> .git/info/sparse-checkout
+echo 'networking/dnssvr/UbuntuServerX86-64/etc_resolv.conf' >> .git/info/sparse-checkout
 
 ## Download with pull, not clone
-git pull origin master
+git pull origin main
 
-echo 'cd into dnssvr/applications/dnssvr/UbuntuServerX86-64 and view details in Dockerfile for building, running, and attaching to the container'
+echo 'cd into dnssvr/networking/dnssvr/UbuntuServerX86-64 and view details in Dockerfile for building, running, and attaching to the container'
 
 # References:
 #   https://terminalroot.com/how-to-clone-only-a-subdirectory-with-git-or-svn
