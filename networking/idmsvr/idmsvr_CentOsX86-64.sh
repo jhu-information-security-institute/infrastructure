10f68d6059a7d3cadb98566044eb4b305ea2b6e6
reubenajohnston <reuben.a.johnston@gmail.com>
2024-03-02 17:51:41 -0500

    Update idmsvr_CentOsX86-64.sh

--- networking/idmsvr/idmsvr_CentOsX86-64.sh
+++ networking/idmsvr/idmsvr_CentOsX86-64.sh
@@ -11,7 +11,7 @@ git init
 git config pull.rebase false  # merge (the default strategy)
 
 # Track repository, do not enter subdirectory
-git remote add -f origin https://github.com/jhu-information-security-institute/NwSec.git
+git remote add -f origin https://github.com/jhu-information-security-institute/infrastructure.git
 
 # Enable the tree check feature
 git config core.sparseCheckout true
@@ -20,13 +20,13 @@ git config core.sparseCheckout true
 # That is inside the hidden .git directory that was created
 # by running the command: git init
 # And inside it enter the name of the specific files (or subdirectory) you only want to clone
-echo 'applications/ipasvr/CentOsX86-64/Dockerfile' >> .git/info/sparse-checkout
-echo 'applications/ipasvr/README.md' >> .git/info/sparse-checkout
+echo 'networking/idmsvr/CentOsX86-64/Dockerfile' >> .git/info/sparse-checkout
+echo 'networking/idmsvr/README.md' >> .git/info/sparse-checkout
 
 ## Download with pull, not clone
-git pull origin master
+git pull origin main
 
-echo 'cd into ipasvr/applications/ipasvr/CentOsX86-64 and view details in Dockerfile for building, running, and attaching to the container'
+echo 'cd into idmsvr/networking/idmsvr/CentOsX86-64 and view details in Dockerfile for building, running, and attaching to the container'
 
 # References:
 #   https://terminalroot.com/how-to-clone-only-a-subdirectory-with-git-or-svn
