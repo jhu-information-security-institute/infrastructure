ff9ae4ae4b916e0cc9627c3542d96ff6ba643195
reubenajohnston <reuben.a.johnston@gmail.com>
2024-03-02 17:20:48 -0500

    Update suricata-KaliX86-64.sh

--- networking/suricata/suricata-KaliX86-64.sh
+++ networking/suricata/suricata-KaliX86-64.sh
@@ -11,7 +11,7 @@ git init
 git config pull.rebase false  # merge (the default strategy)
 
 # Track repository, do not enter subdirectory
-git remote add -f origin https://github.com/jhu-information-security-institute/NwSec.git
+git remote add -f origin https://github.com/jhu-information-security-institute/infrastructure.git
 
 # Enable the tree check feature
 git config core.sparseCheckout true
@@ -20,18 +20,18 @@ git config core.sparseCheckout true
 # That is inside the hidden .git directory that was created
 # by running the command: git init
 # And inside it enter the name of the specific files (or subdirectory) you only want to clone
-echo 'applications/suricata/KaliX86-64/.project' >> .git/info/sparse-checkout
-echo 'applications/suricata/KaliX86-64/Dockerfile' >> .git/info/sparse-checkout
-echo 'applications/suricata/KaliX86-64/etc_default_suricata' >> .git/info/sparse-checkout
-echo 'applications/suricata/KaliX86-64/etc_suricata_suricata.yaml' >> .git/info/sparse-checkout
-echo 'applications/suricata/KaliX86-64/var_lib_suricata_rules_test-ping.rules' >> .git/info/sparse-checkout
-echo 'applications/suricata/KaliX86-64/var_lib_suricata_rules_test-dos.rules' >> .git/info/sparse-checkout
-echo 'applications/suricata/KaliX86-64/var_lib_suricata_rules_test-mail.rules' >> .git/info/sparse-checkout
+echo 'networking/suricata/KaliX86-64/.project' >> .git/info/sparse-checkout
+echo 'networking/suricata/KaliX86-64/Dockerfile' >> .git/info/sparse-checkout
+echo 'networking/suricata/KaliX86-64/etc_default_suricata' >> .git/info/sparse-checkout
+echo 'networking/suricata/KaliX86-64/etc_suricata_suricata.yaml' >> .git/info/sparse-checkout
+echo 'networking/suricata/KaliX86-64/var_lib_suricata_rules_test-ping.rules' >> .git/info/sparse-checkout
+echo 'networking/suricata/KaliX86-64/var_lib_suricata_rules_test-dos.rules' >> .git/info/sparse-checkout
+echo 'networking/suricata/KaliX86-64/var_lib_suricata_rules_test-mail.rules' >> .git/info/sparse-checkout
 
 ## Download with pull, not clone
-git pull origin master
+git pull origin main
 
-echo 'cd into suricata/applications/suricata/KaliX86-64 and view details in Dockerfile for building, running, and attaching to the container'
+echo 'cd into suricata/networking/suricata/KaliX86-64 and view details in Dockerfile for building, running, and attaching to the container'
 
 # References:
 #   https://terminalroot.com/how-to-clone-only-a-subdirectory-with-git-or-svn
