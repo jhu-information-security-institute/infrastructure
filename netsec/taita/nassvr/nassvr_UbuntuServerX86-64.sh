275e899dc38393b6ea892a4d9ddf956e21d49595
reubenajohnston <reuben.a.johnston@gmail.com>
2024-04-15 07:57:01 -0400

    Update nassvr_UbuntuServerX86-64.sh

--- netsec/taita/nassvr/nassvr_UbuntuServerX86-64.sh
+++ netsec/taita/nassvr/nassvr_UbuntuServerX86-64.sh
@@ -22,7 +22,6 @@ git config core.sparseCheckout true
 # And inside it enter the name of the specific files (or subdirectory) you only want to clone
 echo 'netsec/taita/nassvr/README.md' >> .git/info/sparse-checkout
 echo 'netsec/taita/nassvr/UbuntuServerX86-64/.project' >> .git/info/sparse-checkout
-echo 'netsec/taita/nassvr/UbuntuServerX86-64/Dockerfile' >> .git/info/sparse-checkout
 echo 'netsec/taita/nassvr/UbuntuServerX86-64/etc_exports' >> .git/info/sparse-checkout
 echo 'netsec/taita/nassvr/UbuntuServerX86-64/etc_fstab' >> .git/info/sparse-checkout
 echo 'netsec/taita/nassvr/UbuntuServerX86-64/etc_default_tftpd-hpa' >> .git/info/sparse-checkout
@@ -30,7 +29,7 @@ echo 'netsec/taita/nassvr/UbuntuServerX86-64/etc_default_tftpd-hpa' >> .git/info
 ## Download with pull, not clone
 git pull origin main
 
-echo 'cd into nassvr/netsec/taita/nassvr/UbuntuServerX86-64 and view details in Dockerfile for building, running, and attaching to the container'
+echo 'cd into nassvr/netsec/taita/nassvr/UbuntuServerX86-64 and view details in README.md'
 
 # References:
 #   https://terminalroot.com/how-to-clone-only-a-subdirectory-with-git-or-svn
